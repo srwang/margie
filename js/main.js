0cd3158caa12df44f98e6b064a7881cb8765c051
Sharon Wag <sharon.ran.wang@gmail.com>
2016-11-03 23:58:18 -0400

    not breaking on safari

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
+//keep image height uniform to avoid blinking links
+window.onload = function(){
+    var imgWidth = $('.swapped-img-position').width();
+    $('.swapped-img-position').height(imgWidth * 0.72);
+};
+
+$(window).resize(function(){ 
+    var imgWidth = $('.swapped-img-position').width();
+    $('.swapped-img-position').height(imgWidth * 0.72);
+})
 
 
 $(document).ready(function(){
-
-    $(window).resize(function(){ //keep image height uniform to avoid blinking links
-        var imgWidth = $('.swapped-img-position').width();
-        $('.swapped-img-position').height(imgWidth * 0.72);
-    })
-
     //gallery objects
     var lithoGal = [{src:'Aransas_I&2', caption: ['Aransas I & II','Hand colored Linocut','12&quot; x 9&quot;']},
                     {src: 'Cliff SwallowsI',caption: ['Cliff Swallows I','Lithograph 11','1/2&quot; x 16&quot;']},
